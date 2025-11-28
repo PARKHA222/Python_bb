1e80976a9a0979ff4f32ff1e71cb83696b02b47d
Park Ha <2501077@student.hywoman.ac.kr>
2025-11-28 11:02:14 +0900

    2501077 박하의 과제입니다.

---
+++ 2501077.py
@@ -0,0 +1,89 @@
+# 과제 39
+def b(i):
+  for num in range(0,i+1):
+    if num % 2 != 0:
+      print (num, end=' '"\n")
+  
+x = int(input('정수를 입력하세요:'))
+b(x)
+
+# 과제 40
+def b(i):
+  for num in range(0,i+1):
+    if num % 3 == 0:
+      print (num, end=' '"\n")
+
+x = int (input('정수를 입력하세요:'))
+b(x)
+
+# 과제 41
+def b(n1, n2, n3, n4):
+
+    max = n1
+    min = n1
+  
+    if n2 > max:
+        max = n2
+    if n2 < min:
+        min = n2
+        
+    if n3 > max:
+        max = n3
+    if n3 < min:
+        min = n3
+      
+    if n4 > max:
+        max = n4
+    if n4 < min:
+        min = n4
+        
+    return max, min
+
+num1 = int(input("첫 번째 숫자를 입력하세요: "))
+num2 = int(input("두 번째 숫자를 입력하세요: "))
+num3 = int(input("세 번째 숫자를 입력하세요: "))
+num4 = int(input("네 번째 숫자를 입력하세요: "))
+
+max_value, min_value = b(num1, num2, num3, num4)
+    
+print(f"최댓값: {max_value}")
+print(f"최솟값: {min_value}")
+
+# 과제 42
+def b(i): # 39 과제랑 같음
+  for num in range(0,i+1):
+    if num % 2 != 0:
+      print (num, end=' '"\n")
+  
+x = int (input('정수를 입력하세요:'))
+b(x)
+
+# 과제 43
+def b(i):
+  m =1
+  for z in range(1, i+1):
+    m *= z
+
+  return m
+
+x = int (input("0보다 크거나 같은 정수 n을 입력하세요: "))
+print (b(x),end='!'"\n")
+
+# 과제 44
+def b(v,z):
+  for i in range(v+1):
+    for j in range(z+1):
+      if (i*j > 30):
+        print ("%d * %d = %d" % (i,j,i*j))
+
+v = int(input('2이상 9이하의 숫자를 입력하세요:'))
+z = int(input('2이상 9이하의 숫자를 입력하세요:'))
+b(v,z)
+
+# 과제 45
+def b(z, x, h, n, m):
+  return z + x + h + n + m
+
+y, u, i, o, p = list(map(int, input('1 2 3 4 5를 입력하세요: ').split()))
+rst = b(y, u, i, o, p)
+print (f"합계: [{rst}]")
